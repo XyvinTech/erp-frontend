69e54c9840ece717dd5c4be9c5094b2210327d15
Fasilfaz <fazsby2001@gmail.com>
2025-03-29 19:30:29 +0530

    dist added

--- src/api/api.js
+++ src/api/api.js
@@ -14,7 +14,7 @@ api.interceptors.request.use(
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-    return config;
+    return config; 
   },
   (error) => Promise.reject(error)
 );
